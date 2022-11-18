f8956579acd8e2353c077352f082dad96ae03c05
expertglacier <118358272+expertglacier@users.noreply.github.com>
2022-11-18 13:01:49 +0700

    submit homeworks

---
+++ exercise_1/bin/paractise_1.dart
@@ -0,0 +1,7 @@
+//`Đây là kết quả buổi học thứ 2 về Dart: DART BASIC (phần 1)`
+
+void main() {
+    String str = " đây là kết quả của buổi học thứ 2 về dart: dart basics (phần 1)...";
+
+    print("Đây ${str.substring(5, 15)} ${str.substring(20, 37)} Dart: DART BASIC ${str.substring(56, 64)}");
+}
---
+++ exercise_1/test/paractise_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:paractise_1/paractise_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
